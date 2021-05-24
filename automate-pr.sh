8ee0f9eda08230753f7a2c943e603044c2d0d7e9
Ernesto Ojeda <ernesto.ojeda@intel.com>
2021-05-24 13:22:25 -0700

    fix: remove echo
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- automate-pr.sh
+++ automate-pr.sh
@@ -3,8 +3,8 @@
 count=${1:-3}
 label=${2:-bug}
 
-echo git checkout -b "US1144-$count"
-echo touch "bugfix-$count"
+git checkout -b "US1144-$count"
+touch "bugfix-$count"
 git add . && git c "fix: bug $count"
 git push origin "$(git rev-parse --abbrev-ref HEAD)"
 new_pr=$(gh pr create -a ernestojeda -b main -H "$(git rev-parse --abbrev-ref HEAD)" -f -l "$label")
