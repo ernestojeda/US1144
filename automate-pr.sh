a783da9a14e0f0e4eed9bbf46e044f0ea3919b6d
Ernesto Ojeda <ernesto.ojeda@intel.com>
2021-05-24 13:06:32 -0700

    script to automate pr creation
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

---
+++ automate-pr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+count=${1:-3}
+label=${2:-bug}
+
+echo git checkout -b "US1144-$count"
+echo touch "bugfix-$count"
+# git add . && git c "fix: bug $count"
+# git push origin "$(lb)"
+# new_pr=$(gh pr create -a ernestojeda -b main -H "$(lb)" -f -l $label)
+# pr_number=$(basename "$new_pr")
+# gh pr merge "${pr_number}" -m
+# git checkout main
+# git pull
+
